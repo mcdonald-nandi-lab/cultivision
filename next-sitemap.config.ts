9d55c43bb5576a3fe50f6df5798f4e9ec631ab8a
aunshx <aunsh.sb@gmail.com>
2025-05-08 12:08:25 -0700

    Sitemap and metadata

---
+++ next-sitemap.config.ts
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://aunshx.github.io/cultivision",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  generateIndexSitemap: false,
+};
