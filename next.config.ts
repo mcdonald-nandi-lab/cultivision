48b7d2281720b8ff4bd2c8c2c284ebe5f7a77884
aunshx <aunsh.sb@gmail.com>
2025-05-07 23:01:29 -0700

    next js config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/cultivision", // This is the repo name as we are using GH pages to host this web app
+  assetPrefix: "/cultivision",
+  images: {
+    unoptimized: true, // required for static image export
+  },
 };
 
 export default nextConfig;
