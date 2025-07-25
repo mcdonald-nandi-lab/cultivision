318cd9a7e7a8f510e858f1be48eb9ee8b0c13d3e
aunshx <aunsh.sb@gmail.com>
2025-07-24 17:02:18 -0700

    Removing userback feedstock

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,7 +86,6 @@ const RootLayout = ({
           {children}
           <CookieConsent />
           <ConditionalAnalytics />
-          {/* <FeedbackButton /> */}
         </ComposeProviders>
       </body>
     </html>
